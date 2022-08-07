a6756913f5ef769f166e470a75082bd80d9aea7f
Alexander <alexander.martinez@lapatota.cl>
2022-08-07 16:42:20 -0400

    useRef - Primer uso

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWidthCustomHook } from "./02-useEffect/FormWidthCustomHook";
-import { MulitipleCustomHooks } from "./03-examples/MulitipleCustomHooks";
+// import { MulitipleCustomHooks } from "./03-examples/MulitipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <MulitipleCustomHooks />
+  <FocusScreen />
   // </React.StrictMode>
 );
