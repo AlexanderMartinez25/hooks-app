89c48d1b351e6c4e634a5ac83c1cdeb5275a887a
Alexander <alexander.martinez@lapatota.cl>
2022-08-07 17:23:14 -0400

    Memo - Método de React

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import ReactDOM from "react-dom/client";
 // import { FormWidthCustomHook } from "./02-useEffect/FormWidthCustomHook";
 // import { MulitipleCustomHooks } from "./03-examples/MulitipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+import { Memorize } from "./06-memos/Memorize";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <Layout />
+  <Memorize />
   // </React.StrictMode>
 );
