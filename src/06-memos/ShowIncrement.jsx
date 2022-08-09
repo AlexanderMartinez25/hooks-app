97fa0a627d98cc45a4032790423a3ccb567a2ec4
Alexander <alexander.martinez@lapatota.cl>
2022-08-08 21:55:34 -0400

    132. useCallback

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// y por eso se dispara solo una ves este componente (esat memorizado)
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log("me volví a generar :(");
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        increment();
+      }}
+    >
+      Incrementar
+    </button>
+  );
+});
