0cc921f13d0e429a81016b90a653b793e6306469
Alexander <alexander.martinez@lapatota.cl>
2022-08-08 22:17:21 -0400

    Tarea Memorize

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { HooksApp } from "./HooksApp";
@@ -10,12 +11,12 @@ import ReactDOM from "react-dom/client";
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
-import { CallBackHook } from "./06-memos/CallBackHook";
+// import { CallBackHook } from "./06-memos/CallBackHook";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <CallBackHook />
+  <Padre />
   // </React.StrictMode>
 );
