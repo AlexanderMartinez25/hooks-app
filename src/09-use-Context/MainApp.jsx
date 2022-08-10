47b25e796f0ac632157d373582a88fb273da5406
Alexander <alexander.martinez@lapatota.cl>
2022-08-10 15:28:17 -0400

    Link

--- src/09-use-Context/MainApp.jsx
+++ src/09-use-Context/MainApp.jsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
 
 export const MainApp = () => {
   return (
     <>
       <h1>Main App</h1>
+      <NavBar />
       <hr />
 
       <Routes>
