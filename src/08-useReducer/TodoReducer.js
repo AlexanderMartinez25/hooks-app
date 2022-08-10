15303681577c04ec06ed4101fb21f18be0569535
Alexander <alexander.martinez@lapatota.cl>
2022-08-09 22:34:04 -0400

    Toggle Todo - Marcar como completado o pendiente un TODO

--- src/08-useReducer/TodoReducer.js
+++ src/08-useReducer/TodoReducer.js
@@ -6,6 +6,17 @@ export const todoReducer = (initailState = [], action) => {
     case "[TODO] Remove Todo":
       return initailState.filter((todo) => todo.id !== action.payload);
 
+    case "[TODO] Toggle Todo":
+      return initailState.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        return todo;
+      });
+
     default:
       return initailState;
   }
