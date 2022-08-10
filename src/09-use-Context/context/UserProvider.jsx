c39fbf320816e542bf48b6acbc59503ce94665dc
Alexander <alexander.martinez@lapatota.cl>
2022-08-10 19:29:18 -0400

    useContext

--- src/09-use-Context/context/UserProvider.jsx
+++ src/09-use-Context/context/UserProvider.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
-const user = {
-  id: 23,
-  name: "Alexander Martínex",
-  email: "alex@gmail.com",
-};
+// const user = {
+//   id: 23,
+//   name: "Alexander Martínex",
+//   email: "alex@gmail.com",
+// };
 
 export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: "Muundo", user }}>
+    // <UserContext.Provider value={{ hola: "Muundo", user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
