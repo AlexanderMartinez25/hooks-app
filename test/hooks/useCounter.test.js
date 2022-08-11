0095c5df2b9ad7d11af14991aa546d76c0f5f9b2
Alexander <alexander.martinez@lapatota.cl>
2022-08-10 21:36:02 -0400

    Ejecutar funciones del customHook (pruebas)

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
 
 describe("Pruebas en el useCounter", () => {
@@ -16,6 +16,45 @@ describe("Pruebas en el useCounter", () => {
 
   test("debe de generar el counter con el valor de 100", () => {
     const { result } = renderHook(() => useCounter(100));
+    expect(result.current.counter).toBe(100);
+  });
+
+  test("debe de incrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+
+    // ejecutar funcion y esperar resultado: "act"
+    act(() => {
+      // ejecutamos la funcion
+      increment();
+      increment(2);
+    });
+
+    expect(result.current.counter).toBe(103);
+  });
+
+  test("debe de decrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      // ejecutamos la funcion
+      decrement();
+      decrement(2);
+    });
+
+    expect(result.current.counter).toBe(97);
+  });
+
+  test("debe de realizar el rest", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { reset, decrement } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+
     expect(result.current.counter).toBe(100);
   });
 });
